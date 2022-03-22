4aa60d6c524339ec9f14a8930944b653f5f26b0f
timkim <timk@adobe.com>
2022-03-21 18:46:08 -0700

    Debug

--- downloadMetadata.js
+++ downloadMetadata.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 const path = require('path');
 const download = require('download');
 
-const directoryPath = path.join(__dirname, 'static/attask/api');
+const directoryPath = path.join(__dirname, 'static/attask/');
 
 const getAllFiles = dir =>
   fs.readdirSync(dir).reduce((files, file) => {
@@ -17,51 +17,36 @@ var result = getAllFiles(directoryPath);
 console.log(result);
 console.log();
 
-// result.forEach(apiMetaFile => {
-//   fs.readFile(apiMetaFile, "utf8", (err, jsonString) => {
-//     if (err) {
-//       console.log("File read failed:", err);
-//       return;
-//     }
-  
-//     let fileDataJson = JSON.parse(jsonString);  
-//     for (let [apiKey, apiValue] of Object.entries(fileDataJson.data.objects)) {
-//       for (let [apiInnerKey, apiInnerValue] of Object.entries(apiValue)) {
-//         if(apiInnerKey === 'url'){
-//           console.log(`${apiInnerKey}: ${apiInnerValue}`);
-//         }
-//       }
-//     }
-//   });
-// });
-
-
-fs.readFile(result[0], "utf8", (err, jsonString) => {
-  if (err) {
-    console.log("File read failed:", err);
-    return;
-  }
-
-  let fileDataJson = JSON.parse(jsonString);
-
-  for (let [apiKey, apiValue] of Object.entries(fileDataJson.data.objects)) {
-    for (let [apiInnerKey, apiInnerValue] of Object.entries(apiValue)) {
-      if(apiInnerKey === 'url'){
-        console.log(`${apiInnerKey}: ${apiInnerValue}`);
-
-        let urlPath = apiInnerValue.split('/metadata')[0];
-
-        let lastIndex = urlPath.lastIndexOf('/');
-        let code = urlPath.slice(lastIndex + 1)
-
-        let downloadPath = result[0].split('/metadata.json')[0] + '/' + code + '.json';
-
-        console.log(`download ${apiInnerValue} to ${downloadPath}`);
-        
-        (async () => {
-          fs.writeFileSync(downloadPath, await download(apiInnerValue));
-        })();
+result.forEach(apiMetaFile => {
+  if(apiMetaFile.indexOf('.json' >= 0)){
+    fs.readFile(apiMetaFile, "utf8", (err, jsonString) => {
+      if (err) {
+        console.log("File read failed:", err);
+        return;
       }
-    }
+  
+      let fileDataJson = JSON.parse(jsonString);
+  
+      for (let [apiKey, apiValue] of Object.entries(fileDataJson.data.objects)) {
+        for (let [apiInnerKey, apiInnerValue] of Object.entries(apiValue)) {
+          if(apiInnerKey === 'url'){
+            console.log(`${apiInnerKey}: ${apiInnerValue}`);
+    
+            let urlPath = apiInnerValue.split('/metadata')[0];
+    
+            let lastIndex = urlPath.lastIndexOf('/');
+            let code = urlPath.slice(lastIndex + 1)
+    
+            let downloadPath = apiMetaFile.split('/metadata.json')[0] + '/' + code + '.json';
+            
+            // (async () => {
+            //   fs.writeFileSync(downloadPath, await download(apiInnerValue));
+            // })();
+          }
+        }
+      }
+    });
   }
 });
+
+
