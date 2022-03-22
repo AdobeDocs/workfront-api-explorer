53d079bbfc81fb953a1ac76c99332552fc3d2e69
timkim <timk@adobe.com>
2022-03-21 18:03:15 -0700

    Testing download

--- downloadMetadata.js
+++ downloadMetadata.js
@@ -29,13 +29,48 @@ var result = getAllFiles(directoryPath);
 console.log(result);
 console.log();
 
+// result.forEach(apiMetaFile => {
+//   fs.readFile(apiMetaFile, "utf8", (err, jsonString) => {
+//     if (err) {
+//       console.log("File read failed:", err);
+//       return;
+//     }
+  
+//     let fileDataJson = JSON.parse(jsonString);  
+//     for (let [apiKey, apiValue] of Object.entries(fileDataJson.data.objects)) {
+//       for (let [apiInnerKey, apiInnerValue] of Object.entries(apiValue)) {
+//         if(apiInnerKey === 'url'){
+//           console.log(`${apiInnerKey}: ${apiInnerValue}`);
+//         }
+//       }
+//     }
+//   });
+// });
+
 
 fs.readFile(result[0], "utf8", (err, jsonString) => {
   if (err) {
     console.log("File read failed:", err);
     return;
   }
-  //console.log("File data:", jsonString);
+
   let fileDataJson = JSON.parse(jsonString);
-  console.log(fileDataJson.data.objects)
+
+  for (let [apiKey, apiValue] of Object.entries(fileDataJson.data.objects)) {
+    for (let [apiInnerKey, apiInnerValue] of Object.entries(apiValue)) {
+      if(apiInnerKey === 'url'){
+        console.log(`${apiInnerKey}: ${apiInnerValue}`);
+
+        let urlPath = apiInnerValue.split('/metadata')[0];
+
+        let lastIndex = urlPath.lastIndexOf('/');
+        let code = urlPath.slice(lastIndex + 1)
+
+        let downloadPath = result[0].split('/metadata.json')[0] + '/' + code + '.json';
+
+        console.log(`download ${apiInnerValue} to ${downloadPath}`)
+        download(apiInnerValue, )
+      }
+    }
+  }
 });
